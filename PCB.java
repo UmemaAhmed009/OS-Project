b86f0e901fb949e3978662578ba3c23b35862902
Umema Ahmed <umemaahmed009@gmail.com>
2022-11-02 16:33:07 +0500

    Added Exceptions Class and started Mem Management

--- PCB.java
+++ PCB.java
@@ -1,14 +1,40 @@
+import java.io.*;
 public class PCB {
     public int program_Counter;
     public int process_ID;
     public int process_State;
     public int process_Priority;
     public String process_Filename;
+    public FileInputStream Process;
     public int data_Size;
     public int code_Size;
     public int process_Size;
     public register[] gpr_array = new register[16];
     public register[] spr_array = new register[16];
     //public int[] Page_Table;
+
+    public boolean setProcessPriority(int Priority) throws IOException
+    {
+        try{
+            if(Priority>=0 && Priority<=31)
+            {
+                this.process_Priority = Priority;
+            }
+            else if(Priority<0 || Priority>31)
+            {
+                throw new OSExceptions();
+            }
+        }
+        catch (OSExceptions e) {
+            System.out.println("The process " + this.process_Filename + " has invalid priority " + Priority + ".");
+            e.InvalidPriority(this.Process);
+            return false;
+            }
+        return true;
+        }
+    public int getProcessPriority()
+    {
+        return this.process_Priority;
+    }
     
 }
--- driverFile.java
+++ driverFile.java
@@ -1,21 +1,22 @@
 import java.io.*;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+//import java.util.PriorityQueue;
+//import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
 public class driverFile{
     public static byte[] mainMemory = new byte[65536];
     public static byte[] code;
     public static void main(String[] args) throws IOException {
+        Pages[] abbasi_main_memory = new Pages[6];
         int i = 1; //temporary variable
-        int Que1count = 0;
-        int Que2count = 0;
+        int Queue1_count = 0;
+        int Queue2_count = 0;
         int byteRead = 0;
         Boolean Que2more = false;
         boolean quantumQ1 = false;
-        int clocktime = 12;
-        int[] arraysort = new int[5];
-        int temp;
+        //int clocktime = 12;
+        int[] arraysort = new int[5]; //sorted array
+        int temp; //temporary variable used in sorting
    /*     for (int q = 0; q < 5; q++) {
             System.out.print("enter number");
             // Scanner sc = new Scanner(System.in);
@@ -79,31 +80,45 @@ public class driverFile{
         PCB tempPCB = new PCB(); //temp
         tempPCB.process_Priority = 0;
 
-        File [] filenames=new File[6];
-        File Folder = new File("D:\\semster5\\os\\DemoFiles");
+        File [] filenames;
+        File Folder = new File("E:\\5th Semester\\OS\\demoFiles");
         filenames = Folder.listFiles();
+
         //Program Loading
         for(int f=0; f< filenames.length; f++)
         {
             System.out.println("The file is: " + filenames[f]);
         }
+        System.out.println("");
+        boolean boolValue = true;
         for(int counter=0;counter<6;counter++) {
-
-
             try {
                 FileInputStream inStream = new FileInputStream(filenames[counter]);
                 i=1;
                 PCB PCB = new PCB();
+                PCB.Process = inStream;
                 PCB.process_Filename = filenames[counter].getName();
                 System.out.println("The filename is: " + PCB.process_Filename);
                 //DataInputStream dataInputStr = new DataInputStream(inStream);
                 while ((byteRead = inStream.read()) != -1) //checks whether we have reached the end of the file or not
                 {
+                    if(PCB.process_Filename.equals("power"))
+                    {   
+                        System.out.println("");
+                        System.out.println("The value of i is: " + i);
+                        System.out.println("The byte read is: " + byteRead);
+                        boolValue = PCB.setProcessPriority(32);
+                        System.out.println("The value of boolean is: " + boolValue);
+                        if(boolValue == false)
+                        {
+                            break;
+                        }
+                    }
                     //Total memory size = 64 Kb
 
                     /* --Loading Program into memory-- */
                     /* --Storing values in PCB--  */
-                    if (i == 1) {
+                    else if (i == 1) {
                         PCB.process_Priority = byteRead;
                         System.out.println("The process priority is: " + PCB.process_Priority);
                     } else if (i == 2) {
@@ -120,16 +135,20 @@ public class driverFile{
                     /* --Adding to the ready queue-- */
                     i++;
                 }
-                PCB.process_Size = i;
+                System.out.println("The last byte read is: " + byteRead);
+                PCB.process_Size = i + 1;
                 System.out.println("The process size is: " + PCB.process_Size);
                 PCB.code_Size = PCB.process_Size - PCB.data_Size - 8;
                 System.out.println("The code size is: " + PCB.code_Size);
+
+
             } catch (FileNotFoundException e) {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
             System.out.println();
         }
+
         /* Add error handling for priority && cpu sceduling
             for(PCB process : Queue_1){
         if(PCB.process_Priority>=0 && PCB.process_Priority<=15) {
